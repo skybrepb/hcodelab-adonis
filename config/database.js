2ae97e5da802f4c37d2f35a980905a75a551f72d
Evandro Pereira <evandro@optiplex.com.br>
2020-06-02 19:50:48 -0300

    BAnco mySQL

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const Url = require('url-parse')
+//const Url = require('url-parse')
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
 /** Cria uma instancia do cleardb no mysql (Gratuito) Heroku*/
-const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+//const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
@@ -61,13 +61,16 @@ module.exports = {
       // user: Env.get('DB_USER', 'root'),
       // password: Env.get('DB_PASSWORD', ''),
       // database: Env.get('DB_DATABASE', 'adonis')
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
-  
-    },
+      // host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      // port: Env.get('DB_PORT', ''),
+      // user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
+      // password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
+      // database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+      host: "us-cdbr-east-05.cleardb.net",
+      user: "b5412e08d8468d",
+      password: "9479261d",
+      database: "heroku_0be97d671790a17"
+      },
     debug: Env.get('DB_DEBUG', false)
   },
 
